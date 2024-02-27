2131105ec028cd7f3c94ddf6c84d94bd001f444f
lucasmainstdevifal <lpc3@aluno.ifal.edu.br>
2024-02-26 21:18:01 -0300

    chore: updates

--- 2. Busca e Ordenação - Exercícios/1.buscaseq.c
+++ 2. Busca e Ordenação - Exercícios/1.buscaseq.c
@@ -15,15 +15,15 @@ int main() {
     int tamanho = sizeof(numeros);
     
     int chave;
-    printf("Digite um número dentro das opções {10,7,25,14,3}: ");
+    printf("Digite um numero dentro das opcoes {10,7,25,14,3}: ");
     scanf("%d", &chave);
     
     // %d é inteiro , %f é float, %c char
     
     int resultado = buscaSequencial(numeros, tamanho, chave);
     if (resultado == -1)
-        printf("O elemento %d não foi encontrado no array.\n", chave);
+        printf("O elemento %d nao foi encontrado no array.\n", chave);
     else
-        printf("O elemento %d foi encontrado na posição %d.\n", chave, resultado);
+        printf("O elemento %d foi encontrado na posicao %d.\n", chave, resultado);
     return 0;
 }
